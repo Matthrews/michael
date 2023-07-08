d7e815b0c891770d745315d6511a643711142002
Oliver <matthrews@outlook.com>
2023-07-08 11:52:51 +0800

    remove unless content

--- main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  var queryStr = location.search;
-  var con = document.getElementById("collection");
-  if (queryStr.indexOf("show=collection") > -1) {
-    con.style.display = "block";
-  }
-};
